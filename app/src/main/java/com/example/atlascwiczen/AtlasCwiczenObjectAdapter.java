d327bae1aa06a647fcc1ea86e28bfc3a6c15be60
karol <karol.malysa@interia.pl>
2022-02-06 16:17:40 +0100

    before onClick

--- app/src/main/java/com/example/atlascwiczen/AtlasCwiczenObjectAdapter.java
+++ app/src/main/java/com/example/atlascwiczen/AtlasCwiczenObjectAdapter.java
@@ -3,9 +3,17 @@ package com.example.atlascwiczen;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AtlasCwiczenObjectAdapter extends RecyclerView.Adapter<AtlasCwiczenObjectAdapter.AtlasCwiczenObjectViewHolder> {
 
@@ -25,7 +33,8 @@ public class AtlasCwiczenObjectAdapter extends RecyclerView.Adapter<AtlasCwiczen
 
     @Override
     public void onBindViewHolder(@NonNull AtlasCwiczenObjectViewHolder holder, int position) {
-
+        AtlasCwiczenObject atlasCwiczenObject = atlasCwiczenObjects[position];
+        holder.setAtlasCwiczenObject(atlasCwiczenObject);
     }
 
     @Override
@@ -36,8 +45,34 @@ public class AtlasCwiczenObjectAdapter extends RecyclerView.Adapter<AtlasCwiczen
 
     class AtlasCwiczenObjectViewHolder extends  RecyclerView.ViewHolder {
 
+        @BindView(R.id.itemImageView)
+        ImageView itemImageView;
+        @BindView(R.id.itemTextView)
+        TextView itemTextView;
+
+
+        private AtlasCwiczenObject atlasCwiczenObject;
+
         public AtlasCwiczenObjectViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+
+            itemTextView.setText(atlasCwiczenObject.getName());
+
+            Glide.with(itemImageView.getContext())
+                    .load("file:///android_assets/" + atlasCwiczenObject.getImage())
+                    .placeholder(R.drawable.cwiczenia_placeholder)
+                    .into(itemImageView);
+
+        }
+
+        public void setAtlasCwiczenObject(AtlasCwiczenObject atlasCwiczenObject) {
+            this.atlasCwiczenObject = atlasCwiczenObject;
+
+        }
+
+        public AtlasCwiczenObject getAtlasCwiczenObject() {
+            return atlasCwiczenObject;
         }
     }
 }
