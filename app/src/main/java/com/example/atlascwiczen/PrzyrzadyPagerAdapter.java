c441f1178cc4a9c0f409f17b34fb60ba997343db
karol <karol.malysa@interia.pl>
2022-02-06 21:24:48 +0100

    adapter cwiczen z przyrzadami

---
+++ app/src/main/java/com/example/atlascwiczen/PrzyrzadyPagerAdapter.java
@@ -0,0 +1,28 @@
+package com.example.atlascwiczen;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class PrzyrzadyPagerAdapter extends FragmentStateAdapter {
+
+    private final AtlasCwiczenObject[] cwiczeniaZPrzyrzadami ;
+
+    public PrzyrzadyPagerAdapter(@NonNull FragmentActivity fragmentActivity, AtlasCwiczenObject[] cwiczeniaZPrzyrzadami) {
+        super(fragmentActivity);
+
+        this.cwiczeniaZPrzyrzadami = cwiczeniaZPrzyrzadami;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return AtlasCwiczenFragment.newInstance(cwiczeniaZPrzyrzadami[position].getPrzyrzady());
+    }
+
+    @Override
+    public int getItemCount() {
+        return cwiczeniaZPrzyrzadami.length;
+    }
+}
